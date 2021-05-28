a2e384c94a5b9328d4da3c55ef3e6fefaba10c96
anwerShahzaib <84967867+anwerShahzaib@users.noreply.github.com>
2021-05-28 15:49:11 +0500

    Delete Class1.java

--- Class1.java
+++
@@ -1,9 +0,0 @@
-package task1;
-public class Class1 extends Thread{
-	
-	public void run(){  
-		   System.out.println("task 1");
-		   //System.out.println("task 5");
-		   //System.out.println("task 6");
-		 }
-}
