9be2915b3126b975f895fdbf3e816ecd2f7d4335
anwerShahzaib <84967867+anwerShahzaib@users.noreply.github.com>
2021-05-28 15:47:53 +0500

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,9 @@
+package task1;
+public class Class1 extends Thread{
+	
+	public void run(){  
+		   System.out.println("task 1");
+		   //System.out.println("task 5");
+		   //System.out.println("task 6");
+		 }
+}
